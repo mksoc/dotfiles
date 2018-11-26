4b112f4c3ccca53beb8c3a2a1a1c3373160ddc9e
Marco Andorno <27402195+mksoc@users.noreply.github.com>
2018-11-26 10:33:45 +0100

    Track i3 config

--- install.sh
+++ install.sh
@@ -5,14 +5,15 @@ dot_dir=~/dotfiles
 bak_dir=~/.dots_bak
 
 # backup old dotfiles
-mkdir $bak_dir
-mv ~/.bashrc $bak_dir
-mv ~/.bash_aliases $bak_dir
-mv ~/.profile $bak_dir
-mv ~/.gitconfig $bak_dir
+mkdir -p $bak_dir ; mv ~/.bashrc $_
+mkdir -p $bak_dir ; mv ~/.bash_aliases $_
+mkdir -p $bak_dir ; mv ~/.profile $_
+mkdir -p $bak_dir ; mv ~/.gitconfig $_
+mkdir -p $bak_dir/.config/i3 ; mv ~/.config/i3/config $_
 
 # symlink dotfiles
 ln -sv $dot_dir/bashrc ~/.bashrc
 ln -sv $dot_dir/bash_aliases ~/.bash_aliases
 ln -sv $dot_dir/profile ~/.profile
 ln -sv $dot_dir/gitconfig ~/.gitconfig
+ln -sv $dot_dir/config/i3/config ~/.config/i3/config
