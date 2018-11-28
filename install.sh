7c6733b57cd5d7067a7d7540705b859379406ddf
Marco Andorno <27402195+mksoc@users.noreply.github.com>
2018-11-29 00:28:05 +0100

    Updated i3 Manjaro

--- install.sh
+++ install.sh
@@ -5,21 +5,21 @@ dot_dir=~/dotfiles
 bak_dir=~/.dots_bak
 
 # backup old dotfiles
-mkdir -p $bak_dir ; mv ~/.bashrc $_
+#mkdir -p $bak_dir ; mv ~/.bashrc $_
 mkdir -p $bak_dir ; mv ~/.bash_aliases $_
-mkdir -p $bak_dir ; mv ~/.profile $_
+#mkdir -p $bak_dir ; mv ~/.profile $_
 mkdir -p $bak_dir ; mv ~/.gitconfig $_
-mkdir -p $bak_dir/.config/i3 ; mv ~/.config/i3/config $_
+mkdir -p $bak_dir/.i3 ; mv ~/.i3/config $_
 mkdir -p $bak_dir/.config/rofi ; mv ~/.config/rofi/config $_
 mkdir -p $bak_dir/.config ; mv ~/.config/compton.conf $_
 mkdir -p $bak_dir/.config/i3blocks ; mv ~/.config/i3blocks/config $_
 
 # symlink dotfiles
-ln -sv $dot_dir/bashrc ~/.bashrc
+#ln -sv $dot_dir/bashrc ~/.bashrc
 ln -sv $dot_dir/bash_aliases ~/.bash_aliases
-ln -sv $dot_dir/profile ~/.profile
+#ln -sv $dot_dir/profile ~/.profile
 ln -sv $dot_dir/gitconfig ~/.gitconfig
-ln -sv $dot_dir/config/i3/config ~/.config/i3/config
+ln -sv $dot_dir/i3/config ~/.i3/config
 mkdir -p ~/.config/rofi ; ln -sv $dot_dir/config/rofi/config $_/config
 ln -sv $dot_dir/config/compton.conf ~/.config/compton.conf
 mkdir -p ~/.config/i3blocks ; ln -sv $dot_dir/config/i3blocks/config $_/config
