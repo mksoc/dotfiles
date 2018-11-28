0c4fbf22388a14df6cc77228c7a149c833500cfc
Marco Andorno <27402195+mksoc@users.noreply.github.com>
2018-11-28 10:51:12 +0100

    Updated i3

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@ mkdir -p $bak_dir ; mv ~/.bash_aliases $_
 mkdir -p $bak_dir ; mv ~/.profile $_
 mkdir -p $bak_dir ; mv ~/.gitconfig $_
 mkdir -p $bak_dir/.config/i3 ; mv ~/.config/i3/config $_
+mkdir -p $bak_dir/.config/rofi ; mv ~/.config/rofi/config $_
+mkdir -p $bak_dir/.config ; mv ~/.config/compton.conf $_
+mkdir -p $bak_dir ; mv ~/.xinitrc $_
 
 # symlink dotfiles
 ln -sv $dot_dir/bashrc ~/.bashrc
@@ -17,3 +20,6 @@ ln -sv $dot_dir/bash_aliases ~/.bash_aliases
 ln -sv $dot_dir/profile ~/.profile
 ln -sv $dot_dir/gitconfig ~/.gitconfig
 ln -sv $dot_dir/config/i3/config ~/.config/i3/config
+mkdir -p ~/.config/rofi ; ln -sv $dot_dir/config/rofi/config $_/config
+ln -sv $dot_dir/config/compton.conf ~/.config/compton.conf
+ln -sv $dot_dir/xinitrc ~/.xinitrc
