c2609155cc59386e2c3e40c784c72c4dff28343e
MIC Training course student 01 <mictrn01@lnxmicv15.cern.ch>
2024-03-01 11:28:06 +0100

    Added neodev

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,6 +7,7 @@ return {
         "neovim/nvim-lspconfig",
     },
     config = function()
+        require("neodev").setup()
         require("lsp-zero").on_attach(function(_, bufnr)
             -- see :help lsp-zero-keybindings
             -- to learn the available actions
